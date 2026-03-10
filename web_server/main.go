cb28c4d050c75604548b64af538fa4ab0a2c8650
seika-afk <artistnoob6@gmail.com>
2026-03-10 22:00:44 +0530

    webserver:added static htmls

---
+++ web_server/main.go
@@ -0,0 +1,21 @@
+package main 
+
+import (
+"fmt"
+"log"
+"net/http"
+)
+
+
+					// /->index.html
+//server->  /hello
+					// /form -> form func ->form.html
+
+func main(){
+
+
+
+ 
+fmt.Println("Hello world")
+
+}
