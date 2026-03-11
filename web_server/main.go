7977f5af754d8de7e969f0af7f194c45c43fc72e
seika-afk <artistnoob6@gmail.com>
2026-03-11 22:06:52 +0530

    addition:: web_server

--- web_server/main.go
+++ web_server/main.go
@@ -11,11 +11,58 @@ import (
 //server->  /hello
 					// /form -> form func ->form.html
 
+
+
+func formhandler (w http.ResponseWriter,r *http.Request){
+	if err := r.ParseForm(); err!=nil{
+
+		fmt.Fprintf(w,"ParseForm() err: %v",err)
+		return
+
+	}
+	name:=r.FormValue("name")
+	address := r.FormValue("address")
+	fmt.Fprintf(w,"Name = %s \n",name)
+
+	fmt.Fprintf(w,"Address= %s \n",address)
+
+
+
+}
+
+func hellohandler(w http.ResponseWriter,r *http.Request){
+if r.URL.Path != "/hello"{
+	http.Error(w,"404 not found",http.StatusNotFound)
+	return 
+	}
+
+if r.Method != "GET"{
+
+http.Error(w,"method is not supported",http.StatusNotFound)
+	return
+
+}
+fmt.Fprintf(w,"hello")
+
+
+
+}
+
 func main(){
+	fileserver := http.FileServer(http.Dir("./static"))
+	http.Handle("/",fileserver)
+		
+	http.HandleFunc("/form",formhandler)
+	http.HandleFunc("/hello",hellohandler)
+
+	fmt.Println("Starting server at port 8080\n")
+	if err:=http.ListenAndServe(":8080",nil);err !=nil{
+					log.Fatal(err)
+
+	}
+	
 
 
 
- 
-fmt.Println("Hello world")
 
 }
